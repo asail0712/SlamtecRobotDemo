16b175a35e58b14eab03d4f30da8ff27d0464109
GrandenCorp <info@granden.rocks>
2025-09-19 18:05:26 +0800

    [Add]Log流程完成

---
+++ Assets/Scripts/UI/Item/LogItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LogItem : MonoBehaviour
+{
+    [SerializeField] private Text showName;
+
+    public void SetLog(string str)
+    {
+        showName.text   = str;
+
+        // 根據字體的 Preferred Size 來調整
+        float height    = showName.preferredHeight;
+
+        (transform as RectTransform).sizeDelta = new Vector2((transform as RectTransform).sizeDelta.x + 20, height + 10);
+    }
+}
---
+++ Assets/Scripts/UI/LogUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+using XPlan.UI;
+using XPlan.Utility;
+
+public class LogUI : UIBase
+{
+    [SerializeField] private GameObject logPrefab;
+    [SerializeField] private GameObject logAnchor;
+
+    private List<GameObject> itemList;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        itemList = new List<GameObject>();
+
+        ListenCall<string>(UICommand.AddMessage, (log) => 
+        {
+            GameObject logGO = GameObject.Instantiate(logPrefab);
+            logAnchor.AddChild(logGO);
+            logGO.transform.SetAsFirstSibling();
+
+            LogItem logItem = logGO.GetComponent<LogItem>();
+            logItem.SetLog(log);
+
+            // log太多 就移除最舊的
+            itemList.Add(logGO);
+            while(itemList.Count > 15)
+            {
+                itemList.RemoveAt(0);
+            }
+        });
+    }
+}
